76bc8a64dfa48bb47b4770aa115e3f45f133f6bc
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2018-08-15 15:49:36 +0900

    Modify install_gsl.sh to replace 'libgsl0ldbl' to 'libgsl2'

--- scripts/install_gsl.sh
+++ scripts/install_gsl.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get -yV install gsl-bin
 sudo apt-get -yV install libgsl0-dbg
 sudo apt-get -yV install libgsl0-dev
-sudo apt-get -yV install libgsl0ldbl
+sudo apt-get -yV install libgsl2
 exit 0
