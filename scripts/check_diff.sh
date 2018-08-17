0ac2164f3271209c327d7fcb6931f5e4dc57d5e0
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2018-08-17 16:06:15 +0900

    Modify check_diff.sh to decorate echo text

--- scripts/check_diff.sh
+++ scripts/check_diff.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-[ -z "$1" ] && echo "Failed: CircleCI API token is NOT found." && exit 1
+text_color_red="\033[37;41;1m"
+text_color_green="\033[37;42;1m"
+text_color_reset="\033[0m"
+
+[ -z "$1" ] && echo "${text_color_red}Failed${text_color_reset} CircleCI API token is NOT found." && exit 1
 
 api_path="https://circleci.com/api/v1.1/project/github/aloerina01/aloerina01.github.io?circle-token=$1&limit=1&offset=0&filter=completed"
 
 revision=$(curl "$api_path" | jq .[0].vcs_revision)
 
-[ -z "$revision" ] && echo "Failed: vcs_revision is empty." && exit 1
+[ -z "$revision" ] && echo "${text_color_red}Failed${text_color_reset} vcs_revision is empty." && exit 1
 
 diffs=$(git diff --name-only $(eval echo $revision)... | grep -E "^.*_posts.*$")
 
 if [ -z "$diffs" ]; then
-  echo "No diff in /_posts/" && exit 1
+  echo "${text_color_green}Success${text_color_reset} No diff in /_posts/" && exit 1
 else
   echo "$diff" && exit 0
 fi
