0a14ed2451035813ec5a30decee7e4703863492b
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2018-10-12 15:45:50 +0900

    Refactoring indexing.sh

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -4,20 +4,47 @@ text_color_red="\033[37;41;1m"
 text_color_green="\033[37;42;1m"
 text_color_reset="\033[0m"
 
-[ -z "$1" ] && echo "${text_color_red}Failed${text_color_reset} CircleCI API token is NOT found." && exit 1
-[ -z "$2" ] && echo "${text_color_red}Failed${text_color_reset} Algolia API key is NOT found." && exit 1
+ci_token=$1
+algolia_token=$2
 
-api_path="https://circleci.com/api/v1.1/project/github/aloerina01/aloerina01.github.io?circle-token=$1&limit=1&offset=0&filter=completed"
+function success () {
+  echo "${text_color_green}Success${text_color_reset} $1"
+}
 
-revision=$(curl "$api_path" | jq .[0].vcs_revision)
+function err () {
+  echo "${text_color_red}Failed${text_color_reset} $1"
+}
 
-[ -z "$revision" ] && echo "${text_color_red}Failed${text_color_reset} vcs_revision is empty." && exit 1
+function validate () {
+  [[ -z "$1" ]] && err "CircleCI API token is NOT found." && exit 1
+  [[ -z "$2" ]] && err "Algolia API key is NOT found." && exit 1
+}
 
-diffs=$(git diff --name-only $(eval echo $revision)... | grep -E "^.*_posts.*$")
+function fetch_revisions () {
+  api_path="https://circleci.com/api/v1.1/project/github/aloerina01/aloerina01.github.io?circle-token=$ci_token&limit=1&filter=completed"
+  cuerrent_result="offset=0"
+  latest_result="offset=2" # build と indexing で2回分のCI結果が積まれるため
+  current_revision=$(curl "$api_path&$cuerrent_result" | jq -r .[0].vcs_revision)
+  latest_revision=$(curl "$api_path&$latest_result" | jq -r .[0].vcs_revision)
+  [[ -z "$current_revision" || -z "$latest_revision" ]] && err "vcs_revision is empty." && exit 1
+  echo "$latest_revision...$current_revision" 
+}
 
-if [ -z "$diffs" ]; then
-  echo "${text_color_green}Success${text_color_reset} No diff in /_posts/" && exit 0
-else
-  echo "$diffs"
-  echo "${text_color_green}Success${text_color_reset} Start indexing." && ALGOLIA_API_KEY=$2 bundle exec jekyll algolia --config ./_config.algolia.yml
-fi
+function check_diff () {
+  revisions=$(cat -)
+  echo $(git diff --name-only $revisions | grep -E "^.*_posts.*$")
+}
+
+function publish_algolia () {
+  diff=$(cat -)
+  echo "diff: $diff"
+  if [[ -z "$diff" ]]; then
+    success "No diff in /_posts/" && exit 0
+  else
+    success "Start indexing." && ALGOLIA_API_KEY=$algolia_token bundle exec jekyll algolia --config ./_config.algolia.yml
+  fi
+}
+
+# main
+validate $ci_token $algolia_token
+fetch_revisions | check_diff | publish_algolia
