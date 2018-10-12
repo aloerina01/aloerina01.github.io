12adaecc9c768f008292a5b7aa93d86238602ae6
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2018-10-12 17:23:19 +0900

    Fix bug

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# ubuntu は /bin/sh が dash ... 
 
-text_color_red="\033[37;41;1m"
-text_color_green="\033[37;42;1m"
-text_color_reset="\033[0m"
+text_color_red="\e[37;41;1m"
+text_color_green="\e[37;42;1m"
+text_color_reset="\e[m"
 
 ci_token=$1
 algolia_token=$2
@@ -16,8 +17,8 @@ err () {
 }
 
 validate () {
-  [[ -z "$1" ]] && err "CircleCI API token is NOT found." && exit 1
-  [[ -z "$2" ]] && err "Algolia API key is NOT found." && exit 1
+  [ -z "$1" ] && err "CircleCI API token is NOT found." && exit 1
+  [ -z "$2" ] && err "Algolia API key is NOT found." && exit 1
 }
 
 fetch_revisions () {
@@ -26,7 +27,7 @@ fetch_revisions () {
   latest_result="offset=2" # build と indexing で2回分のCI結果が積まれるため
   current_revision=$(curl "$api_path&$cuerrent_result" | jq -r .[0].vcs_revision)
   latest_revision=$(curl "$api_path&$latest_result" | jq -r .[0].vcs_revision)
-  [[ -z "$current_revision" || -z "$latest_revision" ]] && err "vcs_revision is empty." && exit 1
+  [ -z "$current_revision" -o -z "$latest_revision" ] && err "vcs_revision is empty." && exit 1
   echo "$latest_revision...$current_revision" 
 }
 
@@ -38,7 +39,7 @@ check_diff () {
 publish_algolia () {
   diff=$(cat -)
   echo "diff: $diff"
-  if [[ -z "$diff" ]]; then
+  if [ -z "$diff" ]; then
     success "No diff in /_posts/" && exit 0
   else
     success "Start indexing." && ALGOLIA_API_KEY=$algolia_token bundle exec jekyll algolia --config ./_config.algolia.yml
