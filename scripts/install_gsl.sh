df9bdef06db9e1b108b6ca2485d6d0461dcb9402
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2019-12-25 15:24:52 +0900

    Update gem & gsl libraries version

--- scripts/install_gsl.sh
+++ scripts/install_gsl.sh
@@ -4,5 +4,6 @@ sudo apt-get update
 sudo apt-get -yV install gsl-bin
 sudo apt-get -yV install libgsl0-dbg
 sudo apt-get -yV install libgsl0-dev
-sudo apt-get -yV install libgsl2
+sudo apt-get -yV install libgsl23
+sudo apt-get -yV install libgslcblas0
 exit 0
