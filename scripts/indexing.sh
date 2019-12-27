b5ab5c1e9c9d454990eef70e002e7b156b28303b
aloerina01 <kiss_mint27@yahoo.co.jp>
2019-12-27 21:58:01 +0900

    Modify to execute indexing with github actions

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -6,9 +6,9 @@ text_color_red="\e[37;41;1m"
 text_color_green="\e[37;42;1m"
 text_color_reset="\e[m"
 
-ci_token=$1
-algolia_token=$2
-github_token=$3
+algolia_token=$1
+github_token=$2
+trigger_sha=$GITHUB_SHA # GitHub Actions default env
 
 success () {
   printf "${text_color_green}Success${text_color_reset} $1\n"
@@ -23,15 +23,14 @@ message () {
 }
 
 validate () {
-  [[ -z "$1" ]] && err "CircleCI API token is NOT found." && exit 1
-  [[ -z "$2" ]] && err "Algolia API key is NOT found." && exit 1
-  [[ -z "$3" ]] && err "GitHub API token is NOT found." && exit 1
+  [[ -z "$1" ]] && err "Algolia API key is NOT found." && exit 1
+  [[ -z "$2" ]] && err "GitHub API token is NOT found." && exit 1
 }
 
 check_force_publish () {
-  [[ -z "$CIRCLE_SHA1" ]] && message "'CIRCLE_SHA1' is empty." && return 1
+  [[ -z "$trigger_sha" ]] && message "trigger_sha' is empty." && return 1
   
-  api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/$CIRCLE_SHA1?token=$github_token"
+  api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/$trigger_sha?token=$github_token"
   is_force=$(curl "$api_path" | jq -r .commit.message | grep -E "^.*\[indexing\].*$")
   if [[ -n "$is_force" ]]; then 
     message "Forced indexing." && return 0
@@ -41,13 +40,10 @@ check_force_publish () {
 }
 
 fetch_revisions () {
-  api_path="https://circleci.com/api/v1.1/project/github/aloerina01/aloerina01.github.io?circle-token=$ci_token&limit=1&filter=completed"
-  cuerrent_result="offset=0"
-  latest_result="offset=2" # build と indexing で2回分のCI結果が積まれるため
-  current_revision=$(curl "$api_path&$cuerrent_result" | jq -r .[0].vcs_revision)
-  latest_revision=$(curl "$api_path&$latest_result" | jq -r .[0].vcs_revision)
-  [[ -z "$current_revision" || -z "$latest_revision" ]] && err "vcs_revision is empty." && exit 1
-  echo "$latest_revision...$current_revision" 
+  api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/gh-actions/20191224/check-runs?status=completed&token=$github_token"
+  latest_sha=$(curl -H "Accept: application/vnd.github.antiope-preview+json" "$api_path" | jq -r .check_runs[0].head_sha)
+  [[ -z "$trigger_sha" || -z "$latest_sha" ]] && err "revisions could not be found." && exit 1
+  echo "$trigger_sha...$latest_sha" 
 }
 
 check_diff () {
@@ -62,10 +58,10 @@ check_diff () {
 }
 
 publish_algolia () {
-  success "Start indexing." && ALGOLIA_API_KEY=$algolia_token bundle exec jekyll algolia --config ./_config.algolia.yml
+  success "Start indexing." # && ALGOLIA_API_KEY=$algolia_token bundle exec jekyll algolia --config ./_config.algolia.yml
 }
 
 # main
-validate "$ci_token" "$algolia_token" "$github_token"
+validate "$algolia_token" "$github_token"
 check_force_publish && publish_algolia | exit
 fetch_revisions | check_diff && publish_algolia || exit 0
