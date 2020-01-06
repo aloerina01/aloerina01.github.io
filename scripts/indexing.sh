4ed4b299a9a3deeabca94d4b491906822200414d
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-01-06 18:57:07 +0900

    Modify indexing.sh

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -41,7 +41,7 @@ check_force_publish () {
 
 fetch_revisions () {
   api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/$trigger_sha/check-runs?status=completed&token=$github_token"
-  latest_sha=$(curl -H "Accept: application/vnd.github.antiope-preview+json" "$api_path" | jq -r .check_runs[0].head_sha)
+  latest_sha=$(curl -H "Accept: application/vnd.github.antiope-preview+json" "$api_path" | jq -r .check_runs[1].head_sha)
   [[ -z "$trigger_sha" || -z "$latest_sha" ]] && err "Revisions could not be found." && exit 1
   echo "$trigger_sha...$latest_sha" 
 }
