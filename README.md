d1197000b72479c41ecfde4e1b8ed10eee35b5ef
あろえりーな <4443321+aloerina01@users.noreply.github.com>
2020-01-07 15:15:57 +0900

    Replaced build system from CiecleCI to GithubActions (#30)
    * Create github workflow config
    
    * Modify gh-actions config to add setup-ruby
    
    * Update gem & gsl libraries version
    
    * Modify deploy.yml to add actions/cache@1
    
    * Modify .gitignore
    
    * Modify README.md
    
    * Modify to execute indexing with github actions
    
    * Modify to remove LSI when indexing
    
    * Modify indexing.sh to use Github Status API
    
    * Remove circle ci config
    
    * Refactoring indexing.sh
    
    * Finishing touches

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# aloerina01.github.io
+<p align="center">
+  <a href="https://aloerina01.github.io/" rel="nofollow">
+    <img width="150" src="https://aloerina01.github.io/assets/logo.png" alt="mf-code">
+  </a>
+</p>
+<h1 align="center">mille-feuille code</h1>
 
-[![CircleCI](https://circleci.com/gh/aloerina01/aloerina01.github.io/tree/development.svg?style=svg)](https://circleci.com/gh/aloerina01/aloerina01.github.io/tree/development)
-
+<p align="center">
+  <img src="https://github.com/aloerina01/aloerina01.github.io/workflows/DEPLOY/badge.svg?branch=development" alt="build status">
+</p>
 
 ## What's this
 [mille-feuille code](https://aloerina01.github.io/)
@@ -24,22 +30,28 @@ This is a blog written about some technical topics, by [Aloerina](https://twitte
 3. `npm start`
 
 
-## Constitution
+## Branch rules
 
 ### development branch
 jekyllベースの構成ファイル(markdownによる記事、_config.yml等)を持つ。記事、Template、デザインなどの追加修正等を行う場合は、必ずこのブランチで(もしくはここからトピックブランチを切り)行う。
 
-`git push`するとCircleCIがrake taskを実行し、静的ページがGenerateされる。
+`push`をトリガーにCIがrake taskを実行すると、静的ページがGenerateされ master にpushされる。
 
 
 
 ### master branch
 
-Generateされたものがmasterブランチにpushされ、それがGithub Pagesとして公開される。
+Github Pagesとして公開される原本。原則CIからのPushのみ。
 
 
-~~See: [Using Jekyll plugins on GitHub Pages](http://ixti.net/software/2013/01/28/using-jekyll-plugins-on-github-pages.html)~~
+## How to work LSI(潜在意味解析)
 
+* To generate related posts list, use `lsi` option in jekyll config.
+* [Classifier Reborn](https://jekyll.github.io/classifier-reborn/) allows you to execute LSI.
+* Optionally, to speed up LSI classification by at least 10x consider installing following libraries.
+  * [GSL - GNU Scientific Library](https://www.gnu.org/software/gsl/)
+  * [Ruby/GSL Gem(rb-gsl)](https://rubygems.org/gems/gsl)
+  * 💡Check GSL dependencies at [Debian packages](https://packages.debian.org/search?searchon=sourcenames&keywords=gsl)
 
 ## Copyright
 
--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-# ubuntu は /bin/sh が dash ... 
+# ubuntu は /bin/sh が dash ...
 
 text_bold="\e[34;47;1m"
 text_color_red="\e[37;41;1m"
 text_color_green="\e[37;42;1m"
 text_color_reset="\e[m"
 
-ci_token=$1
-algolia_token=$2
-github_token=$3
+algolia_token=$1
+github_token=$2
+trigger_sha=$GITHUB_SHA # GitHub Actions default env
+github_status_api="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/development/statuses"
 
 success () {
   printf "${text_color_green}Success${text_color_reset} $1\n"
@@ -23,15 +24,14 @@ message () {
 }
 
 validate () {
-  [[ -z "$1" ]] && err "CircleCI API token is NOT found." && exit 1
-  [[ -z "$2" ]] && err "Algolia API key is NOT found." && exit 1
-  [[ -z "$3" ]] && err "GitHub API token is NOT found." && exit 1
+  [[ -z "$1" ]] && err "Algolia API key is NOT found." && exit 1
+  [[ -z "$2" ]] && err "GitHub API token is NOT found." && exit 1
 }
 
 check_force_publish () {
-  [[ -z "$CIRCLE_SHA1" ]] && message "'CIRCLE_SHA1' is empty." && return 1
+  [[ -z "$trigger_sha" ]] && message "'trigger_sha' is empty." && return 1
   
-  api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/$CIRCLE_SHA1?token=$github_token"
+  api_path="https://api.github.com/repos/aloerina01/aloerina01.github.io/commits/$trigger_sha?token=$github_token"
   is_force=$(curl "$api_path" | jq -r .commit.message | grep -E "^.*\[indexing\].*$")
   if [[ -n "$is_force" ]]; then 
     message "Forced indexing." && return 0
@@ -40,22 +40,13 @@ check_force_publish () {
   fi
 }
 
-fetch_revisions () {
-  api_path="https://circleci.com/api/v1.1/project/github/aloerina01/aloerina01.github.io?circle-token=$ci_token&limit=1&filter=completed"
-  cuerrent_result="offset=0"
-  latest_result="offset=2" # build と indexing で2回分のCI結果が積まれるため
-  current_revision=$(curl "$api_path&$cuerrent_result" | jq -r .[0].vcs_revision)
-  latest_revision=$(curl "$api_path&$latest_result" | jq -r .[0].vcs_revision)
-  [[ -z "$current_revision" || -z "$latest_revision" ]] && err "vcs_revision is empty." && exit 1
-  echo "$latest_revision...$current_revision" 
-}
-
 check_diff () {
-  revisions=$(cat -)
-  message "revisions: $revisions"
-  diff=$(git diff --name-only $revisions | grep -E "^.*_posts.*$")
+  latest_sha=$(curl "$github_status_api" | jq 'map(select( .["state"] == "success")) | .[0].url' | sed -E 's/^.*statuses\/(.*)"$/\1/')
+  message "Revisions: $trigger_sha...$latest_sha"
+  [[ -z "$trigger_sha" || -z "$latest_sha" || "$latest_sha" = "null" ]] && err "Revisions could not be found." && exit 1
+  diff=$(git diff --name-only $trigger_sha...$latest_sha | grep -E "^.*_posts.*$")
   if [[ -z "$diff" ]]; then
-    success "No diff in /_posts/" && return 1
+    success "No diff in /_posts/" && exit 0
   else
     message "Posts are modified." && return 0
   fi
@@ -66,6 +57,6 @@ publish_algolia () {
 }
 
 # main
-validate "$ci_token" "$algolia_token" "$github_token"
-check_force_publish && publish_algolia | exit
-fetch_revisions | check_diff && publish_algolia || exit 0
+validate "$algolia_token" "$github_token"
+check_force_publish && publish_algolia | exit 0
+check_diff && publish_algolia
